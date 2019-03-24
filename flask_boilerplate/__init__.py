bc6078538d3f16927ee7aff080355506947dee74
Zackery Hitchcock <zackeryhitchcock@live.com>
2019-03-24 18:35:08 -0400

    Adds the templates and various updates to files - adds the templades directory within the flask_boilerplate - adds the calculate, hello, index, and method html within the templates directory - adds the layouts directory within the templates directory - adds the base and footer html within the layouts directory - adds aditional tests to test_app.py - adds content to __init__.py to pass the tests added into test_app.py

--- flask_boilerplate/__init__.py
+++ flask_boilerplate/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, abort, make_response
+from flask import Flask, request, abort, make_response, render_template
 
 
 def create_app(test_config=None):
@@ -15,7 +15,7 @@ def create_app(test_config=None):
 
     @app.route('/')
     def index():
-        return 'Hello World!'
+        return render_template('index.html')
 
     @app.route('/hello')
     def hello():
@@ -24,15 +24,37 @@ def create_app(test_config=None):
             print(f"{key}: {value}")
 
         name = request.args.get('name', 'World')
-        return f"Hello {name}!"
+        return render_template('hello.html', name=name)
 
     @app.route('/number/<int:n>')
     def number_route(n):
         return f"Number: {n}"
 
-    @app.route('/method', methods=['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
+    @app.route('/calculate', methods=['GET', 'POST'])
+    def calculate():
+        if request.method == "GET":
+            return render_template('calculate.html', action="Add")
+        elif request.method == "POST":
+            x = float(request.form['x'])
+            y = float(request.form['y'])
+            action = request.form['action']
+
+            if action == "Add":
+                result = x + y
+            elif action == "Subtract":
+                result = x - y
+            elif action == "Multiply":
+                result = x * y
+            elif action == "Divide":
+                result = x / y
+
+        return render_template('calculate.html', result=result, x=x, y=y, action=action)
+
+    method_route_allows=['GET', 'POST', 'PATCH', 'PUT', 'DELETE']
+
+    @app.route('/method', methods=method_route_allows)
     def method_route():
-        return f"HTTP Method: {request.method}"
+        return render_template('method.html', allowed=method_route_allows, method=request.method)
 
     @app.route('/status')
     def status_route():
--- tests/test_app.py
+++ tests/test_app.py
@@ -4,10 +4,7 @@ from flask_boilerplate import create_app
 def test_config():
     assert not create_app().testing
     assert create_app({'TESTING': True}).testing
-
-def test_index(client):
-    response = client.get('/')
-    assert response.data == b'Hello World!'
+    
 
 def test_hello(client):
     response = client.get('/hello')
